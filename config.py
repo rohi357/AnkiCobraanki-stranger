0ba0c31bd85d2a1e411d514082ffad55951577ed
rohi357 <kumarrohit925721@gmail.com>
2025-05-23 15:17:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26328207"))
 API_HASH = getenv("API_HASH", "d6dcc13df7527b36df3a28e04a72b35b")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "7954807388:AAGeH_MMFHWdj3nwBPfkyLVF1PZ7I4rIEGk")
 OWNER_ID = int(getenv("OWNER_ID", "8097369722"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8097369722").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -35,7 +35,7 @@ BOT_USERNAME = os.environ.get("extractor_protxtbot")
 OWNER_ID = int(os.environ.get("OWNER_ID", "8097369722"))
 # ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8097369722, 6126688051").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8097369722").split()))
 # ------------------------------------------------
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "6126688051")
 # ------------------------------------------------
