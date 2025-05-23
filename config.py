cb1e741d36366dc9c7ddbcae1a65f2d227dd3f04
rohi357 <kumarrohit925721@gmail.com>
2025-05-23 20:58:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from os import getenv
 API_ID = int(getenv("API_ID", "21567814"))
 API_HASH = getenv("API_HASH", "cd7dc5431d449fd795683c550d7bfb7e")
 BOT_TOKEN = getenv("BOT_TOKEN", "7954807388:AAGeH_MMFHWdj3nwBPfkyLVF1PZ7I4rIEGk")
-OWNER_ID = int(getenv("OWNER_ID", "8097369722"))
+OWNER_ID = int(getenv("OWNER_ID", "6126688051"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8097369722").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002528316008"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002334419872"))
 PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002583554860")) ""))
 API_HASH = environ.get("API_HASH", "cd7dc5431d449fd795683c550d7bfb7e")
 
@@ -32,14 +32,14 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7954807388:AAGeH_MMFHWdj3nwBPfkyLVF1PZ7
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("extractor_protxtbot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "8097369722"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6126688051"))
 # ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8097369722").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6126688051").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002528316008")
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002334419872")
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://krishna93107437:ecFL2x5KeUnOhHJJ@strangerboy27.h5bsz1t.mongodb.net/")
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002583554860"))
 
